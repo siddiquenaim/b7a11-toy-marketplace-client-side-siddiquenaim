b8263337e660f5523b2176b0cfee1f65fc0e6cc3
siddiquenaim <naim.niter.tex06@gmail.com>
2023-05-19 22:56:22 +0600

    loaded gallery data from database

--- src/pages/Home/Gallery/Gallery.jsx
+++ src/pages/Home/Gallery/Gallery.jsx
@@ -1,32 +1,27 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
 
-const imageUrls = [
-  "https://images.unsplash.com/photo-1651343726950-af1756778738?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8ODZ8fGtpZCUyMGxlZ298ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60",
-  "https://images.unsplash.com/photo-1651551514922-17863f746dbb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=580&q=80",
-  "https://images.unsplash.com/photo-1590922739636-036fb9dfbbeb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8ODl8fGtpZCUyMGxlZ298ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60",
-  "https://images.unsplash.com/photo-1651343726950-af1756778738?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8ODZ8fGtpZCUyMGxlZ298ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60",
-  "https://images.unsplash.com/photo-1631106256072-54c89defe828?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTAxfHxraWQlMjBsZWdvfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
-  "https://images.unsplash.com/photo-1590922739636-036fb9dfbbeb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8ODl8fGtpZCUyMGxlZ298ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60",
-  "https://images.unsplash.com/photo-1651551514922-17863f746dbb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=580&q=80",
-  "https://images.unsplash.com/photo-1651343726950-af1756778738?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8ODZ8fGtpZCUyMGxlZ298ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60",
-  "https://images.unsplash.com/photo-1651343726950-af1756778738?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8ODZ8fGtpZCUyMGxlZ298ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60",
-  "https://images.unsplash.com/photo-1590922739636-036fb9dfbbeb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8ODl8fGtpZCUyMGxlZ298ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60",
-];
-
 const Gallery = () => {
+  const [imageUrls, setImageUrls] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/gallery")
+      .then((res) => res.json())
+      .then((data) => setImageUrls(data));
+  }, []);
   return (
     <div className="my-20">
       <div className="mb-5">
-        <h1 className="text-3xl text-center mb-3 font-bold">Gallery Section</h1>
+        <h1 className="mb-10 text-center text-5xl font-bold">Our Gallery</h1>
       </div>
       <div className="p-3">
         <ResponsiveMasonry columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3 }}>
           <Masonry gutter="20px">
             {imageUrls.map((img, i) => (
               <img
+                className="shadow-2xl"
                 key={i}
-                src={img}
+                src={img.imageURL}
                 style={{ display: "block", cursor: "pointer", width: "100%" }}
                 alt=""
               />
--- src/pages/Home/ShopByCategory/ShopByCategory.jsx
+++ src/pages/Home/ShopByCategory/ShopByCategory.jsx
@@ -17,8 +17,8 @@ const ShopByCategory = () => {
   };
 
   return (
-    <div className=" mx-auto w-[95%] mt-20">
-      <h1 className="mb-10 text-center text-5xl">Shop By Category</h1>
+    <div className=" mx-auto w-[95%] my-20">
+      <h1 className="mb-10 text-center text-5xl font-bold">Shop By Category</h1>
       <div className="text-center w-[100%] mx-auto mb-5 font-bold text-xl">
         <p className="mb-4">Choose which lego you wanna buy</p>
         <div className="lg:flex justify-center items-center space-y-4  lg:space-y-0 lg:space-x-9 ">
